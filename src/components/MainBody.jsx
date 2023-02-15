597b6d32af7768b65b2b99c9a76bfdb8722c71a3
Winter100 <okgu9412@naver.com>
2023-02-15 15:46:43 +0900

    p, span, onRemove

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,6 +1,22 @@
 import style from "./MainBody.module.css";
-const MainBody = () => {
-  return <nav className={style.MainBody}>메인바디</nav>;
+const MainBody = ({ todo, onRemove, onEdit }) => {
+  return (
+    <div className={style.MainBody}>
+      <ul className={style.todoList}>
+        {todo.map((item) => (
+          <p className={style.pLine} key={item.id}>
+            <button className={style.Edit}>수정</button>
+            <span
+              className={style.span}
+              onDoubleClick={() => onRemove(item.id)}
+            >
+              {item.todo}
+            </span>
+          </p>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MainBody;
