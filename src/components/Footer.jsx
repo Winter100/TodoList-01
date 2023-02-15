83ccbc039bad5ef112a7312c6eb44880ff1bb1cb
Winter100 <okgu9412@naver.com>
2023-02-15 15:45:01 +0900

    Add change, submit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import style from "./Footer.module.css";
-const Footer = () => {
+
+const Footer = ({ onAddValue }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onChangeHandler = (e) => {
+    setInputValue(e.target.value);
+  };
   const onSubmitHandler = (e) => {
     e.preventDefault();
+
+    if (inputValue.trim().length === 0) {
+      return;
+    }
+
+    onAddValue(inputValue);
+    setInputValue("");
   };
   return (
     <form className={style.Footer} onSubmit={onSubmitHandler}>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={onChangeHandler}
+        value={inputValue}
+        placeholder="할일을 적어주세요"
+      />
       <p className={style.Button}>
         <button>추가하기</button>
       </p>
