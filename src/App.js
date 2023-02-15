396219f182a5f853ed9de47fe62173085ff118e9
Winter100 <okgu9412@naver.com>
2023-02-15 13:53:29 +0900

    commit Title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Card from "./card/Card";
+import Footer from "./components/Footer";
+import MainBody from "./components/MainBody";
 import Title from "./components/Title";
 
 function App() {
   return (
     <Card>
       <Title />
+      <MainBody />
+      <Footer />
     </Card>
   );
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,6 +1,11 @@
 import style from "./Title.module.css";
 const Title = () => {
-  return <h1 className={style.Title}>Todo List</h1>;
+  return (
+    <>
+      <h1 className={style.Title}>Todo List</h1>
+      <p className={style.year}>2020-02-02</p>
+    </>
+  );
 };
 
 export default Title;
