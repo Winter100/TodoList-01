fad5886175ad3b17b31c493a110cff09a5af5c92
Winter100 <okgu9412@naver.com>
2023-02-15 15:47:22 +0900

    Add Year, Month, Day

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Card from "./card/Card";
 import Footer from "./components/Footer";
@@ -5,11 +6,55 @@ import MainBody from "./components/MainBody";
 import Title from "./components/Title";
 
 function App() {
+  const [todoInput, setTodoInput] = useState([]);
+  const nextId = useRef(0);
+
+  useEffect(() => {
+    const localStorageData = JSON.parse(localStorage.getItem("todo"));
+    setTodoInput(localStorageData);
+
+    if (localStorageData && localStorageData.length > 0) {
+      nextId.current = localStorageData[localStorageData.length - 1].id;
+      console.log(nextId.current);
+    } else {
+      setTodoInput([]);
+    }
+  }, []);
+
+  const onAddValue = (value) => {
+    if (todoInput.length >= 10) {
+      //모달창. 최대 10개까지만 가능
+      return;
+    }
+    const todoArr = [];
+
+    todoArr.push(...todoInput, {
+      id: nextId.current + 1,
+      todo: value,
+      is: false,
+    });
+    setTodoInput(todoArr);
+    localStorage.setItem("todo", JSON.stringify(todoArr));
+    nextId.current += 1;
+  };
+
+  const onremoveHanlder = (id) => {
+    const filterArr = todoInput.filter((item) => item.id !== id);
+    localStorage.setItem("todo", JSON.stringify(filterArr));
+    setTodoInput(filterArr);
+  };
+
+  const onEditHandler = () => {};
+
   return (
     <Card>
       <Title />
-      <MainBody />
-      <Footer />
+      <MainBody
+        todo={todoInput}
+        onRemove={onremoveHanlder}
+        onEdit={onEditHandler}
+      />
+      <Footer onAddValue={onAddValue} />
     </Card>
   );
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,12 @@
 import style from "./Title.module.css";
 const Title = () => {
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth() + 1;
+  const day = new Date().getDate();
   return (
     <>
       <h1 className={style.Title}>Todo List</h1>
-      <p className={style.year}>2020-02-02</p>
+      <p className={style.year}>{`${year}년 ${month}월 ${day}일`}</p>
     </>
   );
 };
